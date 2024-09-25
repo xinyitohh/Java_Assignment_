f621518f3328952b619ad8a69b70eb3f6a5979c0
xinyitohh <xinyitoh74@gmail.com>
2024-09-25 11:11:59 +0800

    ok ezpz

---
+++ src/scheduler/SetImage.java
@@ -0,0 +1,16 @@
+package scheduler;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author yeo yu le
+ */
+import javax.swing.JLabel;
+
+public interface SetImage {
+    void SetImageSize(int size, JLabel...label);
+}
